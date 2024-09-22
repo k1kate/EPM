5147a4207e2f456d499c70371282a0143627e329
k1kate <katya_golovnova@mail.ru>
2024-09-22 20:32:00 +0400

    first commit

---
+++ src/main/java/com/example/EPM/controllers/testController.java
@@ -0,0 +1,13 @@
+package com.example.EPM.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class testController {
+    @GetMapping("")
+    public String page() {
+        return null;
+
+    }
+}
